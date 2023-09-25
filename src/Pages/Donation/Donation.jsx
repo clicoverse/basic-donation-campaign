a9d0223ba19bc89f39a9f842c1abf555cca5a156
clicoverse <mywebchamp@gmail.com>
2023-09-25 19:23:32 +0530

    show all button functioned well

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -5,6 +5,7 @@ import { getDataFromLs } from "../../Utils/LocalStorage";
 const Donation = () => {
   const donations = useLoaderData();
   const [appliedDonations, setAppliedDonations] = useState([]);
+  const [appliedDonationsLength, setAppliedDonationsLength] = useState(4);
 
   useEffect(() => {
     const storedDonationIds = getDataFromLs();
@@ -19,78 +20,91 @@ const Donation = () => {
   }, [donations]);
 
   return (
-    <div className="mx-auto max-w-screen-2xl py-10 sm:px-5 lg:px-10 mb-8 lg:mb-16">
+    <div className="mx-auto flex flex-col justify-center items-center max-w-screen-2xl py-10 sm:px-5 lg:px-10 lg:mb-16">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-        {appliedDonations?.map((appliedDonation) => (
-          <div
-            key={appliedDonation.id}
-            style={{
-              backgroundColor: appliedDonation.color.card_background,
-              borderRadius: "6px",
-              display: "flex",
+        {appliedDonations
+          ?.slice(0, appliedDonationsLength)
+          .map((appliedDonation) => (
+            <div
+              key={appliedDonation.id}
+              style={{
+                backgroundColor: appliedDonation.color.card_background,
+                borderRadius: "6px",
+                display: "flex",
 
-              position: "relative",
-            }}
-          >
-            <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none  bg-clip-border ">
-              <img
-                src={appliedDonation.thumbnail_img}
-                alt="image"
-                className="h-full w-full object-cover"
-              />
-            </div>
-            <div className="p-6">
-              <div className="flex items-center justify-between">
+                position: "relative",
+              }}
+            >
+              <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none  bg-clip-border ">
+                <img
+                  src={appliedDonation.thumbnail_img}
+                  alt="image"
+                  className="h-full w-full object-cover"
+                />
+              </div>
+              <div className="p-6">
+                <div className="flex items-center justify-between">
+                  <p
+                    style={{
+                      backgroundColor: appliedDonation.color.button_background,
+                      padding: "7px",
+                      fontWeight: "600",
+                      borderRadius: "6px",
+                      lineHeight: "1.625",
+                      color: appliedDonation.color.button_text,
+                    }}
+                  >
+                    {appliedDonation.category}
+                  </p>
+                </div>
                 <p
                   style={{
-                    backgroundColor: appliedDonation.color.button_background,
-                    padding: "7px",
+                    color: "black",
                     fontWeight: "600",
-                    borderRadius: "6px",
-                    lineHeight: "1.625",
-                    color: appliedDonation.color.button_text,
+                    margin: "2rem 0",
+                    fontSize: "1.5rem",
                   }}
                 >
-                  {appliedDonation.category}
+                  {appliedDonation.title}
                 </p>
-              </div>
-              <p
-                style={{
-                  color: "black",
-                  fontWeight: "600",
-                  margin: "2rem 0",
-                  fontSize: "1.5rem",
-                }}
-              >
-                {appliedDonation.title}
-              </p>
-              <p
-                style={{
-                  color: appliedDonation.color.title_text,
-                  fontWeight: "600",
-                  margin: "2rem 0",
-                  fontSize: "1.5rem",
-                }}
-              >
-                $ {appliedDonation.price}.00
-              </p>
-              <a className="inline-block" href="#">
-                <button
+                <p
                   style={{
-                    background: appliedDonation.color.title_text,
-                    color: "white",
-                    padding: "8px",
-                    borderRadius: "0.375rem",
+                    color: appliedDonation.color.title_text,
+                    fontWeight: "600",
+                    margin: "2rem 0",
+                    fontSize: "1.5rem",
                   }}
-                  type="button"
                 >
-                  View Details
-                </button>
-              </a>
+                  $ {appliedDonation.price}.00
+                </p>
+                <a className="inline-block" href="#">
+                  <button
+                    style={{
+                      background: appliedDonation.color.title_text,
+                      color: "white",
+                      padding: "8px",
+                      borderRadius: "0.375rem",
+                    }}
+                    type="button"
+                  >
+                    View Details
+                  </button>
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
-        <button className="mx-auto">See All</button>
+          ))}
+      </div>
+      <div
+        className={
+          appliedDonationsLength === appliedDonations.length && "hidden"
+        }
+      >
+        <button
+          onClick={() => setAppliedDonationsLength(appliedDonations.length)}
+          className="px-5 mt-10 py-2 bg-[#009444] border-none text-white font-semibold rounded-md"
+        >
+          See All
+        </button>
       </div>
     </div>
   );
